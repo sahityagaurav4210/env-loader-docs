72c4f39676896ea66aa84c9cf9767682a3c2d044
Gaurav <gauravsahitya.rockymountaintech@gmail.com>
2024-07-02 11:44:41 +0530

    fix: added links
    added github project link

--- src/views/Header.tsx
+++ src/views/Header.tsx
@@ -22,7 +22,6 @@ function Header() {
       document.body.classList.remove("bg-white");
       document.body.classList.add("bg-slate-950");
     };
-
   }, [appTheme]);
 
   return (
@@ -39,16 +38,25 @@ function Header() {
               className="fa-regular fa-moon fa-xl"
               onClick={() => setAppTheme(APP_THEME.LIGHT)}
             ></i>
-            <i className="fa-brands fa-square-github fa-2xl"></i>
+            <i
+              className="fa-brands fa-square-github fa-2xl"
+              onClick={() => {
+                window.open(import.meta.env.VITE_APP_PROJECT_URL, "_blank");
+              }}
+            ></i>
           </div>
-          
         ) : (
           <div className="flex justify-center gap-4">
             <i
               className="fa-regular fa-lightbulb fa-xl"
               onClick={() => setAppTheme(APP_THEME.DARK)}
             ></i>
-            <i className="fa-brands fa-square-github fa-2xl"></i>
+            <i
+              className="fa-brands fa-square-github fa-2xl"
+              onClick={() => {
+                window.open(import.meta.env.VITE_APP_PROJECT_URL, "_blank");
+              }}
+            ></i>
           </div>
         )}
       </header>
